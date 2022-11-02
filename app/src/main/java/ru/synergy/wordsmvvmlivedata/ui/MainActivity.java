9c84300c5b64dbd37281750cb92d2f5627714eac
ATixonov <tixxxonnnofff@rambler.ru>
2022-11-02 18:51:42 +0300

    finished but have some problem

--- app/src/main/java/ru/synergy/wordsmvvmlivedata/ui/MainActivity.java
+++ app/src/main/java/ru/synergy/wordsmvvmlivedata/ui/MainActivity.java
@@ -1,11 +1,23 @@
 package ru.synergy.wordsmvvmlivedata.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Insert;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import ru.synergy.wordsmvvmlivedata.R;
+import ru.synergy.wordsmvvmlivedata.data.room.Word;
 import ru.synergy.wordsmvvmlivedata.domain.WordViewModel;
+import ru.synergy.wordsmvvmlivedata.ui.recycler.WordListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +29,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel = new ViewModelProvider(this, (ViewModelProvider.Factory) new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(WordViewModel.class);
+
+        mWordViewModel.getAllWords().observe(this, words -> {
+            adapter.submitList(words);
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+            startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+        });
+
+        Button button = (Button) findViewById(R.id.delete);
+        button.setOnClickListener(view -> {
+            mWordViewModel.deleteAll();
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.empty_not_saved, Toast.LENGTH_LONG).show();
+        }
     }
 }
