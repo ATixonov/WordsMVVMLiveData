c1d4629c0c5ba6e492d931696fdc1387ee2ee968
ATixonov <tixxxonnnofff@rambler.ru>
2022-11-02 17:54:36 +0300

    continue working

--- app/src/main/java/ru/synergy/wordsmvvmlivedata/ui/MainActivity.java
+++ app/src/main/java/ru/synergy/wordsmvvmlivedata/ui/MainActivity.java
@@ -1,10 +1,17 @@
-package ru.synergy.wordsmvvmlivedata;
+package ru.synergy.wordsmvvmlivedata.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ru.synergy.wordsmvvmlivedata.R;
+import ru.synergy.wordsmvvmlivedata.domain.WordViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
+    public WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
